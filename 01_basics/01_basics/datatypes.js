faadd8b3260bdd2d6b272d5690fbaeb12f93063a
Nilesh panigrahy <nileshpanigrahy49@gmail.com>
2025-09-09 11:03:38 +0000

    config done

---
+++ 01_basics/01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"// treat all js code as newer version
+
+//alert(3+3)// we are using node js not , browsing
+
+let name = "nilesh"
+let age = 20
+let isLoggedin = true
+let city; //undefined
+
+
+// number => 2 to thr power 53
+//bigint
+//string=> ""
+//boolean => true/false
+//null=> stand alone value (represtantation of empty)
+// undefined
+//symbol=> uniqe
+
+
+
+//object
+
+
+console.log(typeof "nilesh");
+
+
---
+++ 01_basics/01_basics/variable.js
@@ -0,0 +1,22 @@
+const accountId = 1234468
+let accountEmail = "nileshpanigrahy49@gmail.com"
+var accountPassword = "129973"
+accountCity = "berhampur"
+// console.table([accountEmail,accountId,accountPassword,accountCity]);
+//accountId = 3//not allowed
+ 
+accountEmail = "dgfjsgf@gasdgjs"
+//accountId = 232197
+accountPassword = "36682"
+accountCity = "bbsr"
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope  ({}-this bracket is known as scope)
+
+
+
+*/
+
+
+console.table([accountEmail,accountPassword,accountCity]);
